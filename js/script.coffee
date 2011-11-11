1e92e3e41089dc7ea89fa5fe54f86682d6b93eb8
Alexander Hill <me@alxhill.com>
2011-11-11 23:23:41 +0000

    It now crashes the browser.

--- js/script.coffee
+++ js/script.coffee
@@ -1,11 +1,21 @@
 $ ->
+  window.randomArray = []
+  ###for i in [1..16]
+    random = Math.ceil Math.random()*15
+    while random in randomArray
+      random = Math.ceil Math.random()*15
+    randomArray.push random
+  ###
+  window.audio = new Audio()
   videosArray = $("video").toArray()
   $("video").click ->
-    if not @paused and not @audio?
+    if not @paused
       @pause()
+      audio.pause()
     else
       $.each videosArray, (i, el) ->
-        el.pause()  unless el.paused
+        el.pause() unless el.paused
 
-      @audio = new Audio()  not @audio?
+      #audio.src = "audio/#{Math.ceil(Math.random()*5)} + .mp3" #Random number from 1 to how many files there are
       @play()
+      audio.play()
--- js/script.js
+++ js/script.js
@@ -1,3 +1,36 @@
-$(function(){
-
-});
+(function() {
+  var __indexOf = Array.prototype.indexOf || function(item) {
+    for (var i = 0, l = this.length; i < l; i++) {
+      if (this[i] === item) return i;
+    }
+    return -1;
+  };
+  $(function() {
+    var i, random, videosArray;
+    window.randomArray = [];
+    for (i = 1; i <= 16; i++) {
+      random = Math.ceil(Math.random() * 15);
+      while (__indexOf.call(randomArray, random) >= 0) {
+        random = Math.ceil(Math.random() * 15);
+      }
+      randomArray.push(random);
+    }
+    window.audio = new Audio();
+    videosArray = $("video").toArray();
+    return $("video").click(function() {
+      if (!this.paused) {
+        this.pause();
+        return audio.pause();
+      } else {
+        $.each(videosArray, function(i, el) {
+          if (!el.paused) {
+            return el.pause();
+          }
+        });
+        audio.src = "" + (Math.ceil(Math.random() * 5)) + " + .mp3";
+        this.play();
+        return audio.play();
+      }
+    });
+  });
+}).call(this);
